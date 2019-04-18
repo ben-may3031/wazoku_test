e316e7f451835355daf3fc4633d706d136c0e9b1
Ben <ben.may@wazoku.com>
2019-04-18 12:49:59 +0100

    save recommendation workings 1

---
+++ scripts/save_recommendations.py
@@ -0,0 +1,64 @@
+'''
+BLURB!!!!
+'''
+import django # isort:skip # noqa
+import os # isort:skip # noqa
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise.settings')  # isort:skip # noqa
+django.setup()  # isort:skip # noqa
+
+import json
+from central import models
+
+
+def get_similarity(
+    idea_feature_vector_1,
+    idea_feature_vector_2,
+):
+    similarity = 0
+
+    for word, tfidf in idea_feature_vector_1.items():
+        similarity += tfidf * idea_feature_vector_2.get(word, default=0)
+
+    return similarity
+
+
+def main():
+    models.Recommendation.objects.all().delete()
+
+    idea_feature_vector_dict = dict(
+        models.IdeaFeatureVector.objects
+        .values_list('idea_id', 'tfidfs')
+    )
+
+    recommendation_objects = []
+
+    for (
+        outer_idea_id,
+        outer_idea_feature_vector_string,
+    ) in idea_feature_vector_dict.items():
+        for (
+            inner_idea_id,
+            inner_idea_feature_vector_string,
+        ) in idea_feature_vector_dict.items():
+            if outer_idea_id == inner_idea_id:
+                continue
+
+            similarity = get_similarity(
+                json.loads(outer_idea_feature_vector_string),
+                json.loads(inner_idea_feature_vector_string),
+            )
+
+            recommendation_objects.append(
+                models.Recommendation(
+                    similarity=similarity,
+                    idea_1=outer_idea_id,
+                    idea_2=inner_idea_id,
+                )
+            )
+
+    models.Recommendation.objects.bulk_create(recommendation_objects)
+
+
+if __name__ == "__main__":
+    main()
